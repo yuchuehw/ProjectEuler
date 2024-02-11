46e6d464a2b1a22dbc1c4f3d5f18a28b5eb19a09
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2024-02-10 23:34:10 -0800

    Update 2.c

--- src/2.c
+++ src/2.c
@@ -2,11 +2,9 @@
 // 1,2,3,5,8,13,21,34,55,89,...
 // By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
 void main(){
-    int sum = 0;
-    int i = 1;
-    int j = 2;
+    int sum = 0, i = 1, j = 2;
     while(i<4e6){
-        sum=i%2?sum:sum+i;
+        sum = i%2 ? sum : sum+i;
         int temp = i+j;
         i = j;
         j = temp;
