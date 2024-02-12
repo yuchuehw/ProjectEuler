6ec450ae3440ed54140a16bf8e7ecf4046d52668
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2024-02-11 18:07:11 -0800

    Create 3.c

---
+++ src/3.c
@@ -0,0 +1,26 @@
+// The prime factors of 13195 are 5, 7, 13, and 29.
+// 
+// What is the largest prime factor of the number 600851475143?
+
+int isPrime(long number){
+    for(long i = 2; i < number; i++){
+        if(number % i == 0){
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void main() {
+    const long C = 600851475143;
+    long c = C;
+    for(long i = 2; i < C; i++){
+        if(c % i == 0 && isPrime(i)) {
+            c /= i;
+            if(c == 1){
+                printf("%ld\n", i);//6857
+                break;
+            }
+        }
+    }
+}
