0e48464e165f1a63ddf129c915288615ea0ff35e
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2024-02-13 14:48:57 -0800

    Create 10.c

---
+++ src/10.c
@@ -0,0 +1,19 @@
+void main() {
+    const int FIRST_K = 2000000;
+    char is_prime[FIRST_K + 1];
+    memset(is_prime, 0, sizeof(is_prime));
+    is_prime[0] = 1;
+    is_prime[1] = 1;
+    long sum = 0; 
+    for (int i = 2; i * i <= FIRST_K; i++) {
+        if (is_prime[i])
+            continue;
+        for (int j = i * i; j <= FIRST_K; j += i)
+            is_prime[j] = 1;
+    }
+    for (int i = 2; i <= FIRST_K; i++) {
+        if (!is_prime[i])
+            sum += i;
+    }
+    printf("%lld\n", sum); // 142913828922
+}
