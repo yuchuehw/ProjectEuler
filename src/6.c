e4f75344f6e78a22a923be9ab61f2f5ac6f773df
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2024-02-18 16:51:14 -0800

    Update 6.c

--- src/6.c
+++ src/6.c
@@ -1,10 +1,22 @@
+// The sum of the squares of the first ten natural numbers is,
+// 1² + 2² + 3² + ... + 10² = 385
+// The square of the sum of the first ten natural numbers is,
+// (1 + 2 + 3 + ... + 10)² = 55² = 3025
+// Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 
+// 3025 - 385 = 2640.
+// Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+// 
+
+# include <stdio.h>
+# define FIRST_K 100
+
 void main() {
-    const int FIRST_K = 100;
+
     int s = 0;
     for(int i=0;i<FIRST_K+1;i++)
         s+=i;
     s*=s;
     for(int i=0;i<FIRST_K+1;i++)
         s-=i*i;
-    printf("%i",s); // 25164150
+    printf("%i\n",s); // 25164150
 }
