e52e70fb731b41e86d8b64b1ff84625e5a9253a9
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2024-02-19 01:00:53 -0800

    Update 12.py

--- src/12.py
+++ src/12.py
@@ -1,17 +1,15 @@
 TARGET = 500
-class P():
-    p = [2]
-    i = 1
-    def primes(self,limit=0):
-        while P.p[-1]<=limit:
-            P.i+=2
-            if all(P.i%j for j in range(2,P.i)):
-                P.p.append(P.i)
-        return P.p
+def primes(limit=0,p=[2,3]): # p act as a static variable here.
+    i = p[-1]
+    while p[-1]<=limit:
+        i+=2
+        if all(i%j for j in range(2,i)):
+            p.append(i)
+    return p
 i = j = 0
 while 1:
     j += 1;i += j;num = i;count = 1
-    for p in P().primes(num**0.5):
+    for p in primes(num**0.5):
         exponent = 0
         while num % p == 0:
             exponent += 1
