b1e51c1af8007ba112f617dbbe789e855dae945e
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2024-02-19 01:01:32 -0800

    Create 14.py

---
+++ src/14.py
@@ -0,0 +1,11 @@
+LIMIT = int(1e6)
+
+def collatz(n, cache={}):# cache act as a static variable here.
+    if n == 1:
+        return 1
+    if n not in cache:
+        cache[n] = 1 + (collatz(3 * n + 1)if n % 2 else collatz(n // 2))
+    return cache[n]
+
+max_pair = max(((collatz(x), x) for x in range(LIMIT // 2, LIMIT)))
+print(max_pair[1])
