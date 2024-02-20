737de197538b5e25a8ebb6761b3ce1c0240d8c66
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2024-02-20 09:01:14 -0800

    Create 15.py

---
+++ src/15.py
@@ -0,0 +1,14 @@
+from functools import cache
+WIDTH = 20
+HEIGHT = 20
+
+@cache
+def lattice_path(x,y,width,height):
+    if x==width and y==height:
+        return 1
+    if x >width or y>height:
+        return 0
+    return lattice_path(x+1,y,width,height)+lattice_path(x,y+1,width,height)
+
+    return cache[(x,y)]
+print(lattice_path(0,0,WIDTH,HEIGHT)) # 137846528820
