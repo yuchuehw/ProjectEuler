fff20ddb0a3e84ae2bc0a4bb83ff586d5550f5a6
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2024-02-21 12:01:24 -0800

    Update 20.py

--- src/20.py
+++ src/20.py
@@ -1,10 +1,14 @@
 import math
-print(sum([int(c) for c in str(math.factorial(100))]))
-# from functools import cache
+TARGET = 100
+print(sum([int(c) for c in str(math.factorial(TARGET))]))
 
+
+####### ALTERNATIVE #######
+# from functools import cache
+# TARGET = 100
 # @cache
 # def factorial(i):
 #     if i ==0:return 1
 #     return i*factorial(i-1)
 
-# print(sum([int(c) for c in str(factorial(100))]))
+# print(sum([int(c) for c in str(factorial(TARGET))]))
