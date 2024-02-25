98fb05b3c768fb295ce1278339add85689ca4a1b
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2024-02-24 17:37:43 -0800

    Update 23.py

--- src/23.py
+++ src/23.py
@@ -1,3 +1,3 @@
-LIMIT = 28123
-s = {i for i in range(LIMIT)if i< sum(j for j in range(1,i)if i%j==0)}
-print(sum(i for i in range(LIMIT)if not any(j for j in s if i-j>0 and i-j in s)))
+LIMIT=28123
+s={i for i in range(LIMIT)if i<sum(j for j in range(1,i)if i%j==0)}
+print(sum(i for i in range(LIMIT)if any(j for j in s if i-j in s)==0))
