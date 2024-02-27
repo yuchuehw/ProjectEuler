b10b245778e65ad7b85e41b6abc7ff42c76976f1
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2024-02-27 12:29:06 -0800

    Update 26.py

--- src/26.py
+++ src/26.py
@@ -1,6 +1,6 @@
-max_num=-1
-max_ = -1
-for j in range(1,1000):
+STOP=1000
+max_=(-1,0)
+for j in range(1,STOP):
 	i =1;i,J=divmod(i,j);l=[0];L=[str(i)];i=J
 	if i:l+=[0];L+=["."]
 	while i:
@@ -8,8 +8,5 @@ for j in range(1,1000):
 		if i in l:L.insert(l.index(i),"(");i=0
 		elif i>=j:l+=[i];i,J=divmod(i,j);L+=[str(i)];i=J
 		else:l+=[i];L+=['0']
-	if "(" in L:
-	    if max_<len(L)-L.index("("):
-	        max_=len(L)-L.index("(")
-	        max_num=j
-print(max_num)
+	max_=max(max_,(len(L)-L.index("("),j))if"("in L else max_
+print(max_[1])
