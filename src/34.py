a2b096fe4dcf4141f6c9848706fdc40ce2949240
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2024-03-01 13:35:15 -0800

    Update 34.py

--- src/34.py
+++ src/34.py
@@ -1,2 +1,2 @@
 import math
-print(sum(i for i in range(3,100000) if sum(math.factorial(int(d))for d in str(i))==i))
+print(sum(i for i in range(3,50000) if sum(math.factorial(int(d))for d in str(i))==i))
