539bd5355e76bff290eb7b378cadba8ec12f5f94
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2024-03-02 11:53:19 -0800

    Create 836_split.py

---
+++ src/836_split.py
@@ -0,0 +1,8 @@
+question="""<p>Let $A$ be an <b>affine plane</b> over a <b>radically integral local field</b> $F$ with residual characteristic $p$.</p>
+
+<p>We consider an <b>open oriented line section</b> $U$ of $A$ with normalized Haar measure $m$.</p>
+
+<p>Define $f(m, p)$ as the maximal possible discriminant of the <b>jacobian</b> associated to the <b>orthogonal kernel embedding</b> of $U$ <span style="white-space:nowrap;">into $A$.</span></p>
+
+<p>Find $f(20230401, 57)$. Give as your answer the concatenation of the first letters of each bolded word.</p>"""
+print("".join([j[0] for j in (" ".join(i[:i.index("<")] for i in question.split("<b>"))[1:]).split(" ")]))
