ff0b32320d6613a3e698e47bfe1e0229e5b1da31
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2024-03-05 10:31:37 -0800

    Update 39.py

--- src/39.py
+++ src/39.py
@@ -1,8 +1,8 @@
 K = 1000
-l=[0]*K
+l=[0]*(K+1)
 for i in range(1,int(K//2)+1):
     for j in range(1,int(K//2)+1):
         k=(i**2+j**2)**0.5
-        if k==int(k) and i+j+k<K:
+        if k==int(k) and i+j+k<=K:
             l[int(i+j+k)]+=1
 print(l.index(max(l)))
