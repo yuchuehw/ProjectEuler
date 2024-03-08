9376f0bb80b0b8c6b19115ee25b2159d627c2c6f
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2024-03-08 15:48:27 -0800

    Update 38.py

--- src/38.py
+++ src/38.py
@@ -1,8 +1,8 @@
 max_=-1
 
-for i in range(10000):
+for i in range(10001):
     s = ""
-    for j in range(1,10):
+    for j in range(1,11):
         prev = s
         s+=str(i*j)
         if "0" in s or len(set(s)) != len(s):
