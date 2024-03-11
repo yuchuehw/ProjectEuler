36754f308259d67e9c64f5f3b866202c305696f8
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2024-03-11 12:21:23 -0700

    Update 28.py

--- src/20-29/28.py
+++ src/20-29/28.py
@@ -5,18 +5,21 @@ g=[[0]*SIZE for i in range(SIZE)]
 
 p = (SIZE//2,)*2
 
-i = iter(range(1,SIZE**2+1))
+i = 0
 
-g[p[0]][p[0]] =next(i)
+g[p[0]][p[0]] = (i:=i+1)
 m = 1
 n = 1
-counter = 0
 for dy,dx in ((0,1),(1,0),(0,-1),(-1,0))*((SIZE-1)//2):
     for _ in range(n):
         p=(p[0]+dy,p[1]+dx)
-        g[p[0]][p[1]] = next(i)
+        g[p[0]][p[1]] = (i:=i+1)
     m,n=(m-1,n)if m else(1,n+1)
-    counter += 1
+
+i = SIZE*SIZE+1
+for j in range(SIZE-1,-1,-1):
+    g[0][j] = (i:=i-1)
 
 g = np.array(g)
-print(sum(np.diag(g[:,::-1])+np.diag(g))-1+SIZE**2)
+print(g)
+print(sum(np.diag(g[:,::-1])+np.diag(g))-g[SIZE//2][SIZE//2])
