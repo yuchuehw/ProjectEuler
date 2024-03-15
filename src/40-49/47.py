b9af8232854bee8136bc42c6d171d4b9519bc40d
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2024-03-15 13:40:16 -0700

    Update 47.py

--- src/40-49/47.py
+++ src/40-49/47.py
@@ -3,7 +3,6 @@ WINDOW = 4
 TARGET = 4
 
 prime = [True]*1_000_000
-
 prime[0]=prime[1]=False
 
 for i in range(2,K):
@@ -14,22 +13,19 @@ for i in range(2,K):
 
 prime_num = [i for i,v in enumerate(prime)if v]
 
-
 for i in range(K):
-    if not any(prime[i:i+WINDOW]):
-        for j in range(i,i+WINDOW):
-            c = 0
-            n=j
-            for p in prime_num:
-                if n%p==0:
-                    c+=1
-                while n%p==0:
-                    n//=p
-                if n==1:
-                    break
-            if c != TARGET:
+    if any(prime[i:i+WINDOW]):
+        continue
+    for j in range(i,i+WINDOW):
+        c=0
+        for p in prime_num:
+            if j==1:
                 break
-        else:
-            print(i)
+            c+=1 if j%p==0 else 0
+            while j%p==0:
+                j//=p
+        if c != TARGET:
             break
-        
+    else:
+        print(i)
+        break
