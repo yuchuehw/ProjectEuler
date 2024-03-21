bddc89d0db68f6cf17bf8dd2e4928cbfa394105b
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2024-03-21 11:20:37 -0700

    Update 10.py

--- src/10-19/10.py
+++ src/10-19/10.py
@@ -1,12 +1,3 @@
 K = 2000000
-
-is_prime = [True]*(K + 1);
-is_prime[0] = is_prime[1] = False
-
-for i,v in enumerate(is_prime):
-    if not v:
-        continue
-    for j in range(i*i,K+1,i):
-        is_prime[j]=False
-
-print(sum(i for i,v in enumerate(is_prime)if v))
+s={0,1}
+print(sum({i for i in range(K)}-{i for i in range(K)if i in s or[s.add(j)for j in range(i*i,K,i)]==1}))
