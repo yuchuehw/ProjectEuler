c7d8ba2ae6d406ace4c978e753099a1fd4345e65
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2024-04-03 16:42:49 -0700

    Update 57.c

--- src/50-59/57.c
+++ src/50-59/57.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #define CHAR_ARRAY_SIZE 1000
 
 int add(char *addent, char const *adder, int const size) {
@@ -8,37 +9,49 @@ int add(char *addent, char const *adder, int const size) {
     char carry = 0;
     int addent_digit = 0;
     int adder_digit = 0;
+    
     for (int i = 0; i < size; i++) {
         int temp = (addent[i] + adder[i] + carry) % 10;
         carry = (addent[i] + adder[i] + carry) / 10;
         addent[i] = temp;
+        
         if (temp) addent_digit = i + 1;
+        
         if (adder[i]) adder_digit = i + 1;
     }
-    return (addent_digit>adder_digit) ? 1 : 0;
+    
+    return (addent_digit > adder_digit) ? 1 : 0;
 }
 
 int main() {
     int answer = 0;
     char *numerator = calloc(CHAR_ARRAY_SIZE, sizeof(char));
     char *denominator = calloc(CHAR_ARRAY_SIZE, sizeof(char));
+    
     numerator[0] = 1;
     denominator[0] = 2;
+    
     char *numerator_ptr = numerator;
     char *denominator_ptr = denominator;
+    
     for (int i = 0; i < 1000; i++) {
         add(numerator_ptr, denominator_ptr, CHAR_ARRAY_SIZE);
         add(numerator_ptr, denominator_ptr, CHAR_ARRAY_SIZE);
+        
         char *temp_ptr = numerator_ptr;
         numerator_ptr = denominator_ptr;
         denominator_ptr = temp_ptr;
+        
         temp_ptr = calloc(CHAR_ARRAY_SIZE, sizeof(char));
         memcpy(temp_ptr, numerator_ptr, CHAR_ARRAY_SIZE * sizeof(char));
+        
         answer += add(temp_ptr, denominator_ptr, CHAR_ARRAY_SIZE);
         free(temp_ptr);
     }
-    printf("%d\n", answer);//153
+    
+    printf("%d", answer); // 153
     free(numerator);
     free(denominator);
+    
     return 0;
 }
