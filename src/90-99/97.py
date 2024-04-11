15eaa5462dc06fbf86230962084065a11215278c
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2024-04-11 08:17:34 -0700

    Update 97.py

--- src/90-99/97.py
+++ src/90-99/97.py
@@ -1,19 +1,6 @@
-import math
-# find the last K_decimal digit of A * 2**B +1
+# find (A * 2**B +1) mod K
 A = 28433
 B = 7830457
-K_DECI = 10
+K = 10**10
 
-# let's do it in binary first.
-K_BIN = int(math.log2(10**K_DECI))+1
-
-binary_equivlent = [int(i) for i in bin(A)[2:]] + [0]*(B-1) + [1]
-
-mod_value = 10**(K_DECI)
-
-a = 0
-
-for i,v in enumerate(reversed(binary_equivlent)):
-    if v:
-        a += ((2**i)%mod_value)
-print(a%mod_value)
+print(sum(2**(j+B)%K for j,v in enumerate(int(i)for i in bin(A)[:1:-1])if v)%K+1)
