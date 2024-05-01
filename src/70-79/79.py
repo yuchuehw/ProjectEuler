5fd8c15f57a0623b283f41753f3e63ef48e311d4
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2024-05-01 12:02:09 -0700

    Update 79.py

--- src/70-79/79.py
+++ src/70-79/79.py
@@ -1,17 +1,22 @@
 log = 319,680,180,690,129,620,762,689,762,318,368,710,720,710,629,168,160,689,716,731,736,729,316,729,729,710,769,290,719,680,318,389,162,289,162,718,729,319,790,680,890,362,319,760,316,729,380,319,728,716
 
-d = {i:set() for i in range(10)}
-seen = []
+d = [[0]*10 for i in range(10)]
+
+seen = [0]*10
 
 for i in log:
     a,b,c = [int(_) for _ in str(i)]
-    d[a].add(b)
-    d[a].add(c)
-    d[b].add(b)
-    seen += [a,b,c]
+    d[a][b] = 1
+    d[a][c] = 1
+    d[b][b] = 1
+    seen[a] = 1
+    seen[b] = 1
+    seen[c] = 1
 
-for k in list(d.keys()):
-    if k not in seen:
-        del d[k]
+d_most = [(sum(v),i) for i,v in enumerate(d)]
 
-print("".join([str(i) for _,i in sorted(zip(list(d.values()),list(d.keys())),reverse=True)]))
+d_most.sort(reverse=True)
+
+for i,v in d_most:
+    if seen[v]:
+        print(v,end="")
